e983593bb6ad1a06cd68e233b4f5d848fc97cbdd
Азиз Газанчиян < >
2017-04-13 19:53:22 +0200

    Updated with call from initial

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import b from 'LIB/b'; // success
-import c from 'LIB/c'; // error
+//import c from 'LIB/c'; // error
 
 console.log(b.init());
+
+b.loadC().then(C => {
+    console.log(C.init()); // success
+});
 
--- b.js
+++ b.js
@@ -1,5 +1,19 @@
 export default {
+
     init() {
         return 'b is here';
+    },
+
+    loadC() {
+
+        return new Promise(resolve => {
+
+            require.ensure([], require => {
+
+                const c = require('./c');
+
+                resolve(c.default);
+            });
+        });
     }
 }
